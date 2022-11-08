80b912c5e7fe56732361632e321ee080b12d1f88
Gabriel Vallejo <gabrielvallejo2000@gmail.com>
2022-11-08 13:45:55 -0600

    animacion ventana

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.new_screen').addClass('change_screen');
+    // function displayHello(){
+    //     $('.change_screen').removeClass('change_screen');
+    //     $('.screen,.new_screen').toggleClass('screen new_screen');
+    //     $('.screen').addClass('change_screen');
+    // }
+    // setInterval(displayHello, 1200);
+});
--- index.php
+++ index.php
@@ -4,19 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CONECTA</title>
+    <title>CONECTA</title>    
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/animations.css">
 </head>
 <body>
-    <div class="screen_load">
-        <div class="logo_area">
-            <div class="logo">
-                <img src="assets/img/logo_white.png">
+    <div class="screen">
+        <div class="screen_load">
+            <div class="logo_area">
+                <div class="logo">
+                    <img src="assets/img/logo_white.png">
+                </div>
+                <div class="logo_title">
+                    CONECTA
+                </div>
             </div>
-            <div class="logo_title">
-                CONECTA
+        </div>
+    </div>
+    <div class="new_screen">
+        <div class="screen_load" style="background:red">
+            <div class="logo_area">
+                <div class="logo">
+                    <img src="assets/img/logo_white.png">
+                </div>
+                <div class="logo_title">
+                    CONECTA
+                </div>
             </div>
         </div>
     </div>
 </body>
+<script src="assets/js/libs/jquery.min.js"></script>
+<script src="assets/js/libs/touch.min.js"></script>
+<script src="assets/js/main.js"></script>
 </html>
