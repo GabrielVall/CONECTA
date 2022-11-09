3c12f9d66a3b6c8ae07312f5f2ee54fa6e5a7238
Gabriel Vallejo <gabrielvallejo2000@gmail.com>
2022-11-09 10:00:03 -0600

    vista sensor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
+function cambiarPantalla(){
+    $('.screen,.new_screen').toggleClass('screen new_screen');
+    $('.change_screen').removeClass('change_screen');
+    $('.new_screen').addClass('change_screen');
+}
 $(function() {
     $('.new_screen').addClass('change_screen');
-    // function displayHello(){
-    //     $('.change_screen').removeClass('change_screen');
-    //     $('.screen,.new_screen').toggleClass('screen new_screen');
-    //     $('.screen').addClass('change_screen');
-    // }
-    // setInterval(displayHello, 1200);
 });
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CONECTA</title>    
+    <title>CONECTA</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/animations.css">
 </head>
@@ -22,13 +22,76 @@
         </div>
     </div>
     <div class="new_screen">
-        <div class="screen_load">
-            <div class="logo_area">
-                <div class="logo">
-                    <img src="assets/img/logo_white.png">
+        <!-- <div class="pantalla_inicio">
+            <div class="bg_svg">
+                <img src="assets/img/fondo.svg">
+            </div>
+            <div class="row">
+                <div class="w-80">
+                    <div class="titulo">
+                        Comparte tus proyectos <span class="main_color">facilmente</span>
+                    </div>
+                    <div class="desc">
+                        Obtén control de tus datos y aprende rápidamente.
+                    </div>
                 </div>
-                <div class="logo_title">
-                    CONECTA
+            </div>
+            <div class="row">
+                <div class="w-80 bottom_div">
+                    <div class="steps">
+                        <div class="step active"> </div>
+                        <div class="step"> </div>
+                        <div class="step"> </div>
+                    </div>
+                    <div class="next_btn_border">
+                        <div class="next_btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                                <path d="M16.7063 10.7063C17.0969 10.3156 17.0969 9.68127 16.7063 9.29065L11.7063 4.29065C11.3156 3.90002 10.6813 3.90002 10.2906 4.29065C9.9 4.68127 9.9 5.31565 10.2906 5.70627L13.5875 9.00002H4C3.44687 9.00002 3 9.4469 3 10C3 10.5531 3.44687 11 4 11H13.5844L10.2937 14.2938C9.90312 14.6844 9.90312 15.3188 10.2937 15.7094C10.6844 16.1 11.3188 16.1 11.7094 15.7094L16.7094 10.7094L16.7063 10.7063Z"/>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="pantalla_inicio">
+            <div class="header_screen">
+                <h2>Proyecto ESP + BMP280</h2>
+            </div>
+            <div class="body_screen">
+                <div class="cards">
+                    <div class="card bg_main">
+                        <div class="top_card">
+                            Sensor BME 280
+                        </div>
+                        <div class="card_title">
+                            Temperatura
+                        </div>
+                        <div class="card_data">
+                            60 C°
+                        </div>
+                    </div>
+                    <div class="card bg_danger">
+                        <div class="top_card">
+                            Sensor BME 280
+                        </div>
+                        <div class="card_title">
+                            Humedad
+                        </div>
+                        <div class="card_data">
+                            20 %
+                        </div>
+                    </div>
+                    <div class="card bg_default">
+                        <div class="top_card">
+                            Sensor BME 280
+                        </div>
+                        <div class="card_title">
+                            Temperatura
+                        </div>
+                        <div class="card_data">
+                            1200 PA
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
